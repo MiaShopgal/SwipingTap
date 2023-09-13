71f1966daa8c590d456005aae7a32aa8dda0ca5e
Mia <Mia.Yu@zykronix.com>
2023-09-13 11:44:21 +0800

    add tabItem for each view in tabView to make the index work at first attempt to navigate from Center to Left by clicking on the "to <" button

--- SwipingTap/ContentView.swift
+++ SwipingTap/ContentView.swift
@@ -21,8 +21,14 @@ struct ContentView : View {
             TabView ( selection : $index ) {
 
                 LeftView ( index : $index ).tag ( 0 )
+                                           .tabItem { Label ( "dummy label",
+                                                              systemImage : "gearshape.fill" ) }
                 CenterView ( index : $index ).tag ( 1 )
+                                             .tabItem { Label ( "label won't appear",
+                                                                systemImage : "gearshape.fill" ) }
                 RightView ( index : $index ).tag ( 2 )
+                                            .tabItem { Label ( "since the style is page",
+                                                               systemImage : "gearshape.fill" ) }
 
             }.tabViewStyle ( .page ( indexDisplayMode : .never ) )
              .onChange ( of : index ) { value in
@@ -39,7 +45,7 @@ struct ContentView_Previews : PreviewProvider {
     static var previews : some View {
 
         ContentView ( index : 1 )
-        
+
     }
 }
 
